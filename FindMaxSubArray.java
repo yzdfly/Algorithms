e51614fa9a697aea8f55ac94dc8e6cce07ea89af
Yzd <sspu4833385@163.com>
2017-07-31 20:19:28 +0800

    add FindMaxSubArray(not complete)

--- FindMaxSubArray.java
+++ FindMaxSubArray.java
@@ -2,7 +2,7 @@ class FindMaxSubArray{
 	public static void main(String[] args) {
 		int[] A= {13,-2,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
 		int[] result = new int[3];
-		FindMaxSubArray1(A,result);
+		FindMaxSubArray3(A,result);
 		for (int i: result) {
 			System.out.print(i+" ");
 		}
@@ -21,6 +21,60 @@ class FindMaxSubArray{
 					maxSum = curSum;
 					i = m;
 					j = n;
+				}
+			}
+		}
+		result[0] = i;
+		result[1] = j;
+		result[2] = maxSum;
+		return result;
+	}
+
+	//分治思想
+	public void FindMaxSubArray2(){
+
+	}
+
+	//最优解
+	public static int[] FindMaxSubArray3(int[] A,int[] result){
+		int m;
+		//记录起始元素的下标
+		int i = 0;
+		//记录终止元素的下标
+		int j = 0;
+		// //记录 上一次maxSum最大时终止元素的下标
+		// int k = 0;
+		//记录curSum又一次从负整数变为正整数时起始元素的下标
+		int flag = 0;
+		//最大子数组和
+		int maxSum = 0;
+		//当前最大子数组和
+		int curSum = 0;
+		for (m=0;m<A.length ;m++ ) {
+			curSum += A[m];
+			if(curSum>maxSum){
+				//当前最大子数组和较大时，更换起始元素下标
+				if (flag != i) {
+					i = flag;
+				}
+				//更换当前终止元素下标
+				j = m;
+				// //更换上一次最大子数组终止元素的下标
+				// if(k!=0||k!=j){
+				// 	k++;
+				// }
+				// if ((j-k)!=1||j==0){
+				// 	k = m;
+				// }
+				maxSum = curSum;
+			}
+			if(curSum<0){
+				curSum = 0;
+				//记录下一个起始元素的下标
+				if (m<(A.length-1)) {
+					if (A[m+1]>0) {
+						flag = m+1;
+					}
 				}
 			}
 		}
